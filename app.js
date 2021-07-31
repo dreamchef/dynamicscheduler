cb33028881f75bc082f31981266b30f5b4b15a77
lys0l <dali9115@colorado.edu>
2021-07-31 10:07:35 -0600

    update readme

---
+++ app.js
@@ -0,0 +1,42 @@
+// --------------------------------
+//          CONFIGURATION
+/// --------------------------------
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const fs = require('fs');
+const https = require("https");
+const axios = require('axios');
+
+app
+  .use(express.static(path.join(__dirname)))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/', (req, res) => res.render('pages/index.ejs'));
+
+
+// --------------------------------
+//          PREFERENCES VIEW ROUTES
+/// --------------------------------
+app.get('/prefs', (req, res) => res.render('pages/prefs'));
+
+
+// --------------------------------
+//          TASKS VIEW ROUTES
+/// --------------------------------
+app.get('/tasks', (req, res) => res.render('pages/tasks'));
+
+
+// --------------------------------
+//          EVENTS VIEW ROUTES
+/// --------------------------------
+app.get('/events', (req, res) => res.render('pages/events'));
+
+
+// --------------------------------
+//          SCHEDULE VIEW ROUTES
+/// --------------------------------
+app.get('/schedule', (req, res) => res.render('pages/schedule'));
