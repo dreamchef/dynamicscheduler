3a71cfcc5ac2185cb349e75654082e3c9fe4d4f7
lys0l <dali9115@colorado.edu>
2021-08-13 09:08:04 -0600

    implented sequelize models

--- app.js
+++ app.js
@@ -1,26 +1,85 @@
-// --------------------------------
-//          CONFIGURATION
-/// --------------------------------
-const express = require('express');
+/////////////////////////////
+//      NODE MODULES       //
+/////////////////////////////
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const express = require('express');
 const app = express();
 const fs = require('fs');
-const https = require("https");
+const https = require('https');
 const axios = require('axios');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
+
+/////////////////////////////
+//     DATABASE MODELS     //
+/////////////////////////////
+class Task extends Model {}
+Task.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: DataTypes.STRING,
+  deadline: DataTypes.DATE,
+  length: DataTypes.INTEGER,
+  type_id: DataTypes.INTEGER,
+  start: DataTypes.DATE,
+  end: DataTypes.DATE
+}, { sequelize, modelName: 'task' });
+
+class Event extends Model {}
+Event.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  start: DataTypes.DATE,
+  end: DataTypes.DATE
+}, { sequelize, modelName: 'event' });
+
+class Pref extends Model {}
+Pref.init({
+  name: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  setting: DataTypes.INTEGER
+}, { sequelize, modelName: 'pref' });
+
+class Type extends Model {}
+Type.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: DataTypes.STRING
+}, { sequelize, modelName: 'type' });
+
+
+/////////////////////////////
+//   APP CONFIGURATION     //
+/////////////////////////////
 app
   .use(express.static(path.join(__dirname)))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+
+/////////////////////////////
+//    HOMEPAGE ROUTES      //
+/////////////////////////////
 app.get('/', (req, res) => res.render('pages/index.ejs'));
 
 
-// --------------------------------
-//          PREFERENCES VIEW ROUTES
-/// --------------------------------
+/////////////////////////////
+//  PREFERENCES ROUTES     //
+/////////////////////////////
 app.get('/prefs', (req, res) => res.render('pages/prefs', {
   numRanges: 1,
   ranges: [{start: '00:00:00', end: '10:00:00'}],
@@ -34,9 +93,9 @@ app.get('/prefs/:ranges', (req, res) => res.render('pages/prefs', {
 }));
 
 
-// --------------------------------
-//          TASKS VIEW ROUTES
-/// --------------------------------
+/////////////////////////////
+//  TASKS VIEW ROUTES      //
+/////////////////////////////
 app.get('/tasks', function(req, res) {
   var tasks = [
     {
@@ -63,9 +122,9 @@ app.get('/tasks', function(req, res) {
 });
 
 
-// --------------------------------
-//          EVENTS VIEW ROUTES
-/// --------------------------------
+////////////////////////////
+//  EVENTS VIEW ROUTES    //
+////////////////////////////
 app.get('/events', function(req, res) {
   var events = [
     {
@@ -82,9 +141,9 @@ app.get('/events', function(req, res) {
 });
 
 
-// --------------------------------
-//          SCHEDULE VIEW ROUTES
-/// --------------------------------
+/////////////////////////////
+//  SCHEDULE VIEW ROUTES   //
+/////////////////////////////
 app.get('/schedule', function(req, res) {
   let events = [
     {
